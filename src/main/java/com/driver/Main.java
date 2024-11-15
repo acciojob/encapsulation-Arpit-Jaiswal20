32a8e81ee4f93c54b4d90573c56672e124f7d76a
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2024-11-15 15:47:46 +0530

    Update Main.java
    solve the problem

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,16 @@
 package com.driver;
 
 public class Main {
-  
+ public  static void main (String []args){
+
+    RWOnly rwonly = new RWOnly();
+
+    //    rwonly.id = 100;
+    //    System.out.println(rwonly.id);
+
+    // java: id has private access in com.driver.RWOnly
+
+        rwonly.setId(100);
+        System.out.println(rwonly.getId());
+ 
 }
