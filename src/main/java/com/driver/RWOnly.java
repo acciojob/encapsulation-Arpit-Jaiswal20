20ead9c3fd862e597e75f5956e4665b9d4d13bd4
Arpit-Jaiswal20 <arpitjaiswalaj9@gmail.com>
2024-11-15 15:48:23 +0530

    Update RWOnly.java
    solve the problem

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+      private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
